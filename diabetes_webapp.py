6da406d1f654a05b3c56c8306371d2a5d17f2b75
vivekabhimanyu <68161639+vivekabhimanyu@users.noreply.github.com>
2023-02-17 21:13:26 +0530

    Update diabetes_webapp.py

--- diabetes_webapp.py
+++ diabetes_webapp.py
@@ -11,7 +11,7 @@ import streamlit as st
 
 
 # loading the saved model
-loaded_model = pickle.load(open("diabetes_model.sav", 'rb'))
+loaded_model = pickle.load(open("diabetes_model.pkl", 'rb'))
 
 #creating a function for prediction
 def diabetes_prediction(input_data):
