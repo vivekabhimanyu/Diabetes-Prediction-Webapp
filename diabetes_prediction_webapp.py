14f936d16fb8da8fc81efcad81c1868397640f44
vivekabhimanyu <68161639+vivekabhimanyu@users.noreply.github.com>
2023-02-22 19:03:09 +0530

    Update diabetes_prediction_webapp.py

--- diabetes_prediction_webapp.py
+++ diabetes_prediction_webapp.py
@@ -13,7 +13,7 @@ import string
 # Adding the background image
 page_bg_image="""
 <style>
-[data-testid="ststyles_streamlitAppContainer__2rBcU"] {background-image:("https://media.istockphoto.com/id/1270360391/vector/multiethnic-nurse-characters-group.jpg?s=1024x1024&w=is&k=20&c=D_cUYZecYTA-5ueRp2ji1MwkrRN5kkKksSFQFXmU20w=");
+[data-testid="stAppViewContainer"] {background-image:("https://media.istockphoto.com/id/1270360391/vector/multiethnic-nurse-characters-group.jpg?s=1024x1024&w=is&k=20&c=D_cUYZecYTA-5ueRp2ji1MwkrRN5kkKksSFQFXmU20w=");
 background-size:cover;
 }
 </style>
