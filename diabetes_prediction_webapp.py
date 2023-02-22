7d4b4a23a0cd59e1e170fe93ccbe1ea8fee2e7e8
vivekabhimanyu <68161639+vivekabhimanyu@users.noreply.github.com>
2023-02-22 22:09:18 +0530

    Update diabetes_prediction_webapp.py

--- diabetes_prediction_webapp.py
+++ diabetes_prediction_webapp.py
@@ -18,7 +18,7 @@ background-color:rgb(255,255,255);}
 [data-testid="stAppViewContainer"] {background-size:cover;
 background-color:rgb(255,255,255);
 background-image-opacity:0.5;
-background-image:url("https://media.giphy.com/media/f3iwJFOVOwuy7K6FFw/giphy.gif");
+background-image:url("https://media.giphy.com/media/U4FkC2VqpeNRHjTDQ5/giphy.gif");
 
 }
 </style>
