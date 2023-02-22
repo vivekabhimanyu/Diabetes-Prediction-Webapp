292e4d60b0af31089f3705e6a80260e8d18fac64
vivekabhimanyu <68161639+vivekabhimanyu@users.noreply.github.com>
2023-02-22 18:13:18 +0530

    Update diabetes_prediction_webapp.py

--- diabetes_prediction_webapp.py
+++ diabetes_prediction_webapp.py
@@ -10,6 +10,15 @@ import pickle
 import streamlit as st
 import string
 
+# Adding the background image
+page_bg_image="""
+<style>
+[data-testid="stAppViewContainer"]{background-color: #e5e5f7;
+opacity: 0.8;
+background-image: radial-gradient(circle at center center, #444cf7, #e5e5f7), repeating-radial-gradient(circle at center center, #444cf7, #444cf7, 10px, transparent 20px, transparent 10px);
+background-blend-mode: multiply;}
+</style>
+"""
 # loading the saved model
 loaded_model = pickle.load(open('trained_model.pkl', 'rb'))
 
