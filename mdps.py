b9a5b94b41f9418cae4702fbefdbf5f1b567f003
vivekabhimanyu <68161639+vivekabhimanyu@users.noreply.github.com>
2023-03-10 12:40:55 +0530

    Update mdps.py

--- mdps.py
+++ mdps.py
@@ -25,7 +25,7 @@ selected =option_menu("Multiple Disesase Prediction System",
                       icons = ["house check","person","activity","bandaid","gender-female"],
                       orientation="horizontal",
                       styles={"container": {"padding": "0!important", "background-color": "#fafafa"},"icon": {"color": "orange", "font-size": "25px"},"nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},"nav-link-selected": {"background-color": "green"},
-                      default_index=0)
+                      default_index=None)
 
 #home vidhi predictions
 if (selected =="Home"):
