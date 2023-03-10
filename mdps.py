7605f6037b1bea5df694600a61937ae3790dd42a
vivekabhimanyu <68161639+vivekabhimanyu@users.noreply.github.com>
2023-03-10 12:41:44 +0530

    Update mdps.py

--- mdps.py
+++ mdps.py
@@ -24,8 +24,7 @@ selected =option_menu("Multiple Disesase Prediction System",
                       options=['Home',"Diabetes","Heart","Covid-19","Breast Cancer"],
                       icons = ["house check","person","activity","bandaid","gender-female"],
                       orientation="horizontal",
-                      styles={"container": {"padding": "0!important", "background-color": "#fafafa"},"icon": {"color": "orange", "font-size": "25px"},"nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},"nav-link-selected": {"background-color": "green"},
-                      default_index=None)
+                      styles={"container": {"padding": "0!important", "background-color": "#fafafa"},"icon": {"color": "orange", "font-size": "25px"},"nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},"nav-link-selected": {"background-color": "green"})
 
 #home vidhi predictions
 if (selected =="Home"):
