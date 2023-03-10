85c5f8b45fada13d8b5b108b978b6f4459ca6b99
vivekabhimanyu <68161639+vivekabhimanyu@users.noreply.github.com>
2023-03-10 12:12:08 +0530

    Update mdps.py

--- mdps.py
+++ mdps.py
@@ -19,7 +19,7 @@ covid19_model = pickle.load(open("covid_model.pkl","rb" ))
 
 breast_cancer_model = pickle.load(open("breast_cancer_model.pkl","rb"))
 
-#sidebar fro navigation
+#sidebar for navigation
 
 with st.sidebar:
     selected =option_menu("Multiple Disesase Prediction System",
@@ -27,6 +27,15 @@ with st.sidebar:
                           ['Home',"Diabetes","Heart","Covid-19","Breast Cancer"],
                           
                           icons = ["house check","person","activity","bandaid","gender-female"],
+                          
+                          orientation='horizantal',
+                          
+                          styles={
+        "container": {"padding": "0!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "green"},
+    }
                           
                           default_index=0)
 
